08159c56d9b6e2a768b8ce7123ca9145c8b4f241
Sushmitha <susmithasanikommu@gmail.com>
2018-07-16 20:29:54 +0530

    Update settings.py

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sm@g)(fbwdh5wc*xe@j++m9rh^uza5se9a57c5ptwkg*b@ki0x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'discussion-forum-clone.herokuapp.com','127.0.0.1']
 
 
 # Application definition
